5395c3899c2d4c881a875f7068b9d5e974b9ecb4
vagnercora <160506431+vagnercora@users.noreply.github.com>
2024-10-16 08:15:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 // Grafico de Pizza
 const ctxPizza =document.getElementById('graficoPizza').getContext('2d');
-const graficoPizza = new Chart(ctxPizza
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['Sim', 'Não'],
+        datasets: [{
+            label: 'Você trabalha?',
+            data: [79, 21],
+            backgroundColor: [
+                '#F05454', '#30475E',
+            ],
+            bordercolor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    Options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Você trabalha?'
+            }
+        }
+    }
+});
+
+const ctxPizza =document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
     type: 'pie',
     data: {
         labels: ['Facebook', 'Instagram','Twitter', 'LinkedIn'],
         datasets: [{
-            label: 'Participação nas redes sociais',
+            label: 'Qual é a sua ocupação?',
             data: [45, 30, 15, 10],
             backgroundColor: [
                 '#F05454', '#30475E', '#DDDDDD', '#222831'
@@ -13,5 +42,19 @@ const graficoPizza = new Chart(ctxPizza
             bordercolor: '#DDDDDD',
             borderWidth: 1
         }]
+    },
+    Options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Qual é a sua ocupação?'
+            }
+        }
     }
-    )
+});
+
+
