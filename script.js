9fc09f6f58c7f441607f63c21e44ee8663b680aa
vagnercora <160506431+vagnercora@users.noreply.github.com>
2024-10-22 08:54:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ const ctxPizza =document.getElementById('graficoPizza').getContext('2d');
 const graficoPizza = new Chart(ctxPizza, {
     type: 'pie',
     data: {
-        labels: ['Facebook', 'Instagram','Twitter', 'LinkedIn'],
+        labels: ['administrativo', 'tecnico','gerencial', 'outros'],
         datasets: [{
             label: 'Qual é a sua ocupação?',
-            data: [45, 30, 15, 10],
+            data: [10, 6, 0, 84],
             backgroundColor: [
                 '#F05454', '#30475E', '#DDDDDD', '#222831'
             ],
@@ -58,3 +58,118 @@ const graficoPizza = new Chart(ctxPizza, {
 });
 
 
+const ctxPizza =document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['menos de 20', '21 a 40','41 a 60', '60 ou mais'],
+        datasets: [{
+            label: 'Quantas horas você trabalha por semana?',
+            data: [57, 43, 0, 0],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            bordercolor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    Options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Qual é a sua ocupação?'
+            }
+        }
+    }
+});
+
+const ctxPizza =document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['muito satisfeito', 'satisfeito','neutro', 'insatifeito'],
+        datasets: [{
+            label: 'Qual é o seu nível de satisfação no trabalho?',
+            data: [26, 10, 52, 12],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            bordercolor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    Options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Qual é a sua ocupação?'
+            }
+        }
+    }
+});
+
+const ctxPizza =document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['remoto', 'presencial','hibrido'],
+        datasets: [{
+            label: 'Qual é o seu tipo de trabalho?',
+            data: [5, 95, 0],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            bordercolor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    Options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Qual é a sua ocupação?'
+            }
+        }
+    }
+});
+
+const ctxPizza =document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['sim', 'não'],
+        datasets: [{
+            label: 'Você participa de treinamentos anualmente?',
+            data: [85, 15],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            bordercolor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    Options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Qual é a sua ocupação?'
+            }
+        }
+    }
+});
